595f11e1a89d0de5f3777f33962db52706eb68a2
Ashley Pliler <ashleypliler1@gmail.com>
2023-06-20 11:32:45 -0600

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,8 +6,8 @@ export const About = () => {
     <div className={styles.container}>
       <div className={styles.background_img}></div>
       <div className={styles.text}>
-      Hello! I am Ashley Pliler and I love Design. Using my imagination to get an idea across in a new and creative way is something I am passionate about. My love of Design drove me to learn more and I graduated from DevMountain Software Bootcamp in full-stack Web-Development. I love front-end web development and creating/designing websites that are aesthetic and easy to navigate. Another way I express my creativity is through photography and videography. I feel like all of these skills, web development, graphic design, and photography are all connected in creating a brand and helping a company reach their goals. I am eager to work with clients to help build their vision.
-      </div>
+
+      Hello! I am Ashley Pliler and I love design. Using my imagination to get an idea across in a new and creative way is something I am passionate about. My love of design drove me to learn more and I graduated from DevMountain Software Bootcamp in full-stack web-development. I love front-end web development and creating/designing websites that are aesthetic and easy to navigate. Another way I express my creativity is through photography and videography. In my experience, these skills; web-development, graphic design, and photography are all connected in creating a brand and helping a company reach their goals. I am eager to work with clients to help build their vision.      </div>
       <div className={styles.bottom}></div>
     </div>
   )
